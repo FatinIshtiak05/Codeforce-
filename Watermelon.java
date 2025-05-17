f73895c52a9252e230d8aa0e98fb9b3a4724ebff
Fatin Ishtiak <122414653+FatinIshtiak05@users.noreply.github.com>
2025-05-17 20:55:42 +0600

    Add files via upload

---
+++ Watermelon.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Watermelon {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.close();
+        if(n == 2)
+            System.out.println("No");
+        else if(n%2 == 0)
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+    }
+}
